c852c83909647bc38e4beb3d9ee87f211d0ebd51
Jagroop Bagri <jagroopbagri@Jagroops-Laptop.local>
2023-06-11 01:47:57 -0700

    add gitignore

--- api/app.js
+++ api/app.js
@@ -1,10 +1,13 @@
 import express from "express";
-const port = 3000;
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Server is ready");
-})
+  res.send("Server is ready");
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
